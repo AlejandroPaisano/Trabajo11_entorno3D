6777a9d3c19d6b40fee8f3d1b4650a6300ff43a3
Dicebreak <alejandro.paisano5463@alumnos.udg.mx>
2024-05-01 01:34:33 -0600

    Commit tarea

---
+++ 3D Plataformer/player assets/Button.gd
@@ -0,0 +1,19 @@
+extends Button
+
+@onready var player=$".."
+@onready var anim=$"../AnimationPlayer"
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_pressed():
+	if player.is_on_floor():
+		player.velocity.y = player.JUMP_VELOCITY
+		anim.play("Jump_Full_Short")
+	pass # Replace with function body.
